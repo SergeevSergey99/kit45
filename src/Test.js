2f4ef8903359f19716fd977a839ecd273fd57429
ProtonSElijah <SElijah@protonmail.com>
2019-11-22 03:27:35 +0300

    Динамическое обновление сигнала и переструктурирование

--- src/Test.js
+++ src/Test.js
@@ -1,29 +1,40 @@
-//import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./ResetBrowser.css";
 import "./Test.css";
 
-let a = 0;
-/**
- * @return {string}
- */
 function Rand_of_var(variant) {
+    let a = 0;
     let signal = "";
-    for (let i = 0; i < 16; i++)
-        signal += sfc32(variant + i) % 2 + "";
+    for (let i = 0; i < 16; i++) {
+        a += variant + i;
+        a = Math.abs(Math.sin(a)) * 10000;
+        signal += Math.floor(a) % 2 + "";
+    }
     a = 0;
     return signal;
 }
-function sfc32(_a) {
+
+/*function sfc32(_a) {
     a +=_a;
     a = Math.abs(Math.sin(a)) * 10000;
     return Math.floor(a);
-}
+}*/
 /*
 function JCreg() {
     return;
 }*/
-var v = 0;
+
 const Test = () => {
+    const [variant, setVariant] = useState("2");
+
+    const submitVariant = e => {
+        alert(variant);
+        e.preventDefault();
+    };
+
+    const changeVariant = e => {
+        setVariant(e.target.value);
+    };
 
     return (
         <div className="App">
@@ -33,10 +44,32 @@ const Test = () => {
             <div className="App-main">
                <div className="App-main-leftside">Лево</div>
                 <div className="App-main-content">
-                    <div className="App-main-content-signal">
-                        <div className="Signal">{Rand_of_var(v)}</div>
-                        <div className="Signal">{v}</div>
-                        <div className="Signal">Сигнал 3</div>
+                   <div className="App-main-content-data">
+                        <div className="App-main-content-signal">
+                            <Signal value={Rand_of_var(variant + 0**2)}/>
+                            <Signal value={Rand_of_var(variant + 1**2)}/>
+                            <Signal value={Rand_of_var(variant + 2**2)}/>
+                            <Signal value={Rand_of_var(variant + 3**2)}/>
+                        </div>
+                        <div className="Scheme">
+
+                        </div>
+                   </div>
+
+                    <div className="App-main-content-variant">
+                        <div className="Variant">
+                           <VariantForm
+                               variant={variant}
+                               submitVariant={submitVariant}
+                               changeVariant={changeVariant}
+                           />
+                        </div>
+                        <div className="Answer">
+
+                        </div>
+                    </div>
+                    <div className="App-main-content-description">
+                        Задание
                     </div>
                 </div>
                 <div className="App-main-rightside">Право</div>
@@ -44,4 +77,18 @@ const Test = () => {
         </div>
     );
 };
+
+const VariantForm = ({variant, submitVariant, changeVariant}) => (
+    <form onSubmit={submitVariant}>
+        <label>
+            <input type="text" value={variant} onChange={changeVariant} />
+        </label>
+        <input type="submit" value="Да!" />
+    </form>
+);
+
+const Signal = ({value}) => (
+    <div className="Signal">{value}</div>
+);
+
 export default Test;
