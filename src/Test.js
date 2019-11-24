5b0b55cb60f14f357877c7ba564c9f1d3ff82926
Sergey <Sergeev_sergey99@mail.ru>
2019-11-24 04:11:50 +0300

    Add very cool signal and etc generator

--- src/Test.js
+++ src/Test.js
@@ -1,16 +1,77 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import "./ResetBrowser.css";
 import "./Test.css";
 
-function Rand_of_var(variant) {
+class JK_Trigger {
+    constructor() {
+        this.signal_c = "";
+        this.signal_j = "";
+        this.signal_k = "";
+        this.signal_r = "";
+        this.signal_s = "";
+        this.inv_C = true;
+        this.inv_R = true;
+        this.inv_S = true;
+        this.order = "RCJKS";
+        this.q = true;
+        this.pass_in = "R";
+        this.pass_val = 0;
+    }
+
+    generate(variant) {
+        this.signal_c = Rand_of_var(variant + 0 ** 2, 20);
+        this.signal_j = Rand_of_var(variant + 1 ** 2);
+        this.signal_k = Rand_of_var(variant + 2 ** 2);
+        this.signal_r = Rand_of_var(variant + 3 ** 2);
+        this.signal_s = Rand_of_var(variant + 4 ** 2);
+
+        let pos = ["C", "J", "K"];
+        this.order = "" + (("1" === Rand_of_var(variant + 11 ** 2, 1)) ? "R" : "S");
+        let tmp = Number(Rand_of_var3(variant + 12 ** 2,1));
+        this.order += pos[tmp];
+        pos.slice(tmp,1);
+        tmp = Number(Rand_of_var(variant + 13 ** 2,1));
+        this.order += pos[tmp];
+        pos.slice(tmp,1);
+        this.order += pos[0];
+        this.order += (("0" === Rand_of_var(variant + 11 ** 2, 1)) ? "R" : "S");
+
+
+        this.inv_C = "1" === Rand_of_var(variant + 5 ** 2, 1);
+        this.inv_R = "1" === Rand_of_var(variant + 6 ** 2, 1);
+        this.inv_S = "1" === Rand_of_var(variant + 7 ** 2, 1);
+        this.q = "1" === Rand_of_var(variant + 8 ** 2, 1);
+        this.pass_in = ("1" === Rand_of_var(variant + 9 ** 2, 1)) ? "R" : "S";
+        this.pass_val = Rand_of_var(variant + 10 ** 2, 1);
+    }
+
+
+}
+
+/**
+ * @return {string}
+ */
+function Rand_of_var(variant, symbols = 19) {
     let a = 0;
     let signal = "";
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < symbols; i++) {
         a += variant + i;
         a = Math.abs(Math.sin(a)) * 10000;
         signal += Math.floor(a) % 2 + "";
     }
-    a = 0;
+    return signal;
+}
+/**
+ * @return {string}
+ */
+function Rand_of_var3(variant, symbols = 19) {
+    let a = 0;
+    let signal = "";
+    for (let i = 0; i < symbols; i++) {
+        a += variant + i;
+        a = Math.abs(Math.sin(a)) * 10000;
+        signal += Math.floor(a) % 3 + "";
+    }
     return signal;
 }
 
@@ -24,15 +85,21 @@ function JCreg() {
     return;
 }*/
 
+var JK_now = new JK_Trigger();
 const Test = () => {
     const [variant, setVariant] = useState("2");
 
+
+    JK_now.generate(variant);
+
     const submitVariant = e => {
         alert(variant);
+        JK_now.generate(variant);
         e.preventDefault();
     };
 
     const changeVariant = e => {
+        JK_now.generate(e.target.value);
         setVariant(e.target.value);
     };
 
@@ -42,27 +109,27 @@ const Test = () => {
                 Шапка
             </div>
             <div className="App-main">
-               <div className="App-main-leftside">Лево</div>
+                <div className="App-main-leftside">Лево</div>
                 <div className="App-main-content">
-                   <div className="App-main-content-data">
+                    <div className="App-main-content-data">
                         <div className="App-main-content-signal">
-                            <Signal value={Rand_of_var(variant + 0**2)}/>
-                            <Signal value={Rand_of_var(variant + 1**2)}/>
-                            <Signal value={Rand_of_var(variant + 2**2)}/>
-                            <Signal value={Rand_of_var(variant + 3**2)}/>
+                            <Signal value={JK_now.signal_c}/>
+                            <Signal value={JK_now.signal_j}/>
+                            <Signal value={JK_now.signal_k}/>
+                            <Signal value={JK_now.signal_r}/>
                         </div>
                         <div className="Scheme">
 
                         </div>
-                   </div>
+                    </div>
 
                     <div className="App-main-content-variant">
                         <div className="Variant">
-                           <VariantForm
-                               variant={variant}
-                               submitVariant={submitVariant}
-                               changeVariant={changeVariant}
-                           />
+                            <VariantForm
+                                variant={variant}
+                                submitVariant={submitVariant}
+                                changeVariant={changeVariant}
+                            />
                         </div>
                         <div className="Answer">
 
@@ -81,9 +148,9 @@ const Test = () => {
 const VariantForm = ({variant, submitVariant, changeVariant}) => (
     <form onSubmit={submitVariant}>
         <label>
-            <input type="text" value={variant} onChange={changeVariant} />
+            <input type="text" value={variant} onChange={changeVariant}/>
         </label>
-        <input type="submit" value="Да!" />
+        <input type="submit" value="Да!"/>
     </form>
 );
 
